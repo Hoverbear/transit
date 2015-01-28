89888bcbc32596b4034334ad257934cdd96bdde6
Andrew Hobden <andrew@hoverbear.org>
2015-01-28 14:34:14 -0800

    Show branches

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ extern crate git2;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate docopt;
 
-use git2::Repository;
+use git2::{Repository, Branch, BranchType};
 use docopt::Docopt;
+use std::old_io as io;
 
 // Write the Docopt usage string.
 static USAGE: &'static str = "
-Usage: transit [options] <repo>
+Usage: transit [options] <repo> [<branch>]
 
 Options:
     -f, --flag  Flags a flag, note the multiple spaces!
@@ -16,6 +17,7 @@ Options:
 #[derive(RustcDecodable, Show)]
 struct Args {
     arg_repo: String,
+    arg_branch: Option<String>,
     flag_flag: bool,
 }
 
@@ -28,12 +30,26 @@ fn main() {
     let path = Path::new(args.arg_repo);
     let repo = Repository::discover(&path)
         .ok().expect("Could not discover a repository.");
-    let mut branches = repo.branches(None)
+    let branch = match args.arg_branch {
+        Some(branch) => repo.find_branch(branch.as_slice(), BranchType::Local),
+        None => choose_branch(&repo),
+    }.ok().expect("Could not get branch.");
+    println!("Repo: {:?}, Branch: {:?}", path, branch.name()
+        .ok().expect("Branch name was not valid UTF-8"));
+}
+
+fn choose_branch(repo: &Repository) -> Result<Branch, git2::Error> {
+    println!("Please choose a branch:");
+    let mut branches = repo.branches(Some(BranchType::Local))
         .ok().expect("Could find any branches.");
     for (branch, variant) in branches {
         let name = branch.name()
             .ok().expect("Could not get branch name.")
             .expect("Branch name was not valid UTF-8");
-        println!("{:?} is {:?}", name, variant);
+        println!("{:?}", name);
     }
+    let chosen = io::stdin().lock().read_to_string()
+        .ok().expect("Could not read from stdin");
+    repo.find_branch(chosen.as_slice(), BranchType::Local)
 }
+
