1a0f9c2f5ed9e73c0396d900a5ecf9fd54bba667
Andrew Hobden <andrew@hoverbear.org>
2015-02-02 14:33:34 -0800

    Show revwalk.

--- src/main.rs
+++ src/main.rs
@@ -29,50 +29,27 @@ fn main() {
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
-
+    // Pull up the revwalk.
     let path = Path::new(args.arg_repo);
     let repo = Repository::discover(&path)
         .ok().expect("Could not discover a repository.");
-
-    // Which branch?
-    let branch = match args.arg_branch {
-        Some(branch) => repo.find_branch(&branch[], BranchType::Local),
-        None => choose_branch(&repo),
-    }.ok().expect("Could not get branch.");
-
-    // Mostly debugging.
-    println!("Repo: {:?}, Branch: {:?}", path, branch.name()
-       .ok().expect("Couldn't get branch name.")
-       .expect("Branch name is not UTF-8."));
-
-    // Get the reference of the branch.
-    let reference = branch.into_reference();
-    let oid = reference.target()
-        .expect("Could not get target.");
-    println!("Target Commit: {:?}", oid);
-
-    let top_commit = repo.find_commit(oid)
-        .ok().expect("Could not get commit.");
-
-    // There might be more than one parent of a commit... Hrm...
-    for commit in top_commit.parents() {
-        println!("Parent: {:?}, {:?}", commit.id(), commit.message()
-            .expect("Could not get commit message"));
+    let mut revwalk = repo.revwalk()
+        .ok().expect("Unable to get revwalk.");
+    // Setup some options.
+    revwalk.simplify_first_parent(); // TODO: Maybe remove?
+    let mut flags = git2::Sort::empty();
+    flags.insert(git2::SORT_TIME);
+    flags.insert(git2::SORT_TOPOLOGICAL);
+    revwalk.set_sorting(flags);
+    // Push HEAD to the revwalk.
+    revwalk.push_head()
+        .ok().expect("Unable to push HEAD.");
+    for id in revwalk {
+        match repo.find_commit(id) {
+            Ok(commit) =>
+                println!("Commit: {:?} - {:?}", commit.id(), commit.message()
+                    .expect("Commit message was not UTF-8.")),
+            Err(error) => (),
+        }
     }
-}
-
-fn choose_branch(repo: &Repository) -> Result<Branch, git2::Error> {
-    println!("Please choose a branch:");
-    let mut branches = repo.branches(Some(BranchType::Local))
-        .ok().expect("Could find any branches.");
-    for (branch, _) in branches {
-        let name = branch.name()
-            .ok().expect("Could not get branch name.")
-            .expect("Branch name was not valid UTF-8");
-        println!("  {}", name);
-    }
-    io::stdout().write_str(&"Choose your branch: "[]);
-    let chosen = io::stdin().lock().read_line()
-        .ok().expect("Could not read from stdin");
-    repo.find_branch(&chosen.trim()[], BranchType::Local)
 }
