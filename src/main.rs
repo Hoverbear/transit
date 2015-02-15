63c7339f92d260d664c6ec8f5abb4d27be4df313
Brody Holden <brody_r_h@hotmail.com>
2015-02-14 19:26:48 -0800

    Print components of diff.

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,8 @@ fn main() {
         let old = Oid::from_str(&old_string[]).and_then(|oid| repo.find_commit(oid));
         let new = Oid::from_str(&new_string[]).and_then(|oid| repo.find_commit(oid));
         if old.is_ok() && new.is_ok() {
-            find_moves(&repo, &old.unwrap(), &new.unwrap()).unwrap();
+            let output = find_moves(&repo, &old.unwrap(), &new.unwrap()).unwrap();
+            make_output(output);
         } else {
             panic!("Commit ids were not valid.");
         }
@@ -73,18 +74,43 @@ fn main() {
 }
 
 fn make_output(output: Vec<Output>) {
-	println!("some json output");
+	println!("make_output:");
+    println!("\toutput.len()={}", output.len());
+    for i in range(0, output.len()) {
+            println!("\told_commit={}",         output[i].old_commit);
+            println!("\tnew_commit={}",         output[i].new_commit);
+            println!("\torigin_line={}",        output[i].origin_line);
+            println!("\tdestintation_line={}",  output[i].destination_line);
+            println!("\tnum_lines={}",          output[i].num_lines);
+    }
+}
 
+
+fn print_or_none(s: &str, opt: Option<u32>) {
+    print!("{}=", s);
+    match opt {
+        Some(value) => println!("{}", value),
+        None => println!("NONE"),
+    }
 }
 
 fn find_moves(repo: &Repository, old: &Commit, new: &Commit) -> Result<Vec<Output>, Error> {
+
+    println!("\nFIND MOVES---------------------");
+
 	let old_tree = try!(old.tree());
 	let new_tree = try!(new.tree());
 	// Build up a diff of the two trees.
 	let diff = try!(Diff::tree_to_tree(repo, Some(&old_tree), Some(&new_tree), None));
 	// State. TODO: Don't just use a u64.
-	let moved = 0;
-	let mut current_hunk: Vec<String> = Vec::new();
+	//let moved = 0;
+
+    //let mut results: Vec<Output> = Vec::new();
+
+    let mut has_addition = false;
+    let mut has_deletion = false;
+
+	//let mut current_hunk: Vec<String> = Vec::new();
 	// Read about this function in http://alexcrichton.com/git2-rs/git2/struct.Diff.html#method.print
 	// It's a bit weird, but I think it will provide the necessary information.
 	diff.print(DiffFormat::Patch, |delta, maybe_hunk, line| -> bool {
@@ -96,23 +122,48 @@ fn find_moves(repo: &Repository, old: &Commit, new: &Commit) -> Result<Vec<Outpu
 		// Filter out all the boring context lines.
 		// If we're not interested in this line just return since it will iterate to the next.
 		if maybe_hunk.is_none() { return true }; // Return early.
+
+        println!("");
+        print_or_none("line.old_lineno()", line.old_lineno());
+        print_or_none("line.new_lineno()", line.new_lineno());
+        println!("line.num_lines()={}",  line.num_lines());
+        println!("line.content_offset={}", line.content_offset());
+        println!("|line.origin()|=|{}|", line.origin());  // Wrap in |'s to detect space characters.
+        print!("line.content()={}", str::from_utf8(line.content()).unwrap());
+
+        println!("delta.nfiles()={}", delta.nfiles());
+        println!("delta.status()={:?}", delta.status());    // :? is for debug trait.
+        println!("delta.old_file():\n\tid={}\n\tpath_bytes=TODO\n\tpath=TODO\n\tsize={}", delta.old_file().id(), delta.old_file().size());
+        println!("delta.new_file():\n\tid={}\n\tpath_bytes=TODO\n\tpath=TODO\n\tsize={}", delta.new_file().id(), delta.new_file().size());
+
 		match line.origin() {
 			// Context
-			' ' | '=' => true,
+			' ' | '=' => {
+                print!("= {}", str::from_utf8(line.content()).unwrap());
+                true
+            },
 			// Headers
-			'F' | 'H' => true,
+			'F' | 'H' => {
+                print!("F {}", str::from_utf8(line.content()).unwrap());
+                true
+            },
 			// Additions
 			'+' | '>' => {
-				println!("+ {}", str::from_utf8(line.content()).unwrap());
+				print!("+ {}", str::from_utf8(line.content()).unwrap());
+                has_addition = true;
 				true
 			},
 			// Deletions
 			'-' | '<' => {
-				println!("- {}", str::from_utf8(line.content()).unwrap());
+				print!("- {}", str::from_utf8(line.content()).unwrap());
+                has_deletion = true;
 				true
 			},
 			// Other (We don't care about these.)
-			_         => true,
+			_         => {
+                print!("_ {}", str::from_utf8(line.content()).unwrap());
+                true
+            }
 		}
 			// Look at that match statement, what a hunk. So hunky.
 			// match maybe_hunk {
@@ -120,13 +171,38 @@ fn find_moves(repo: &Repository, old: &Commit, new: &Commit) -> Result<Vec<Outpu
 			//     None => unimplemented!(),
 			// }
 	});
-	Ok(vec![Output {
-		old_commit: old.id(),
-		new_commit: new.id(),
-		origin_line: 0,
-		destination_line: 0,
-		num_lines: 0
-	}])
+
+
+    println!("\nhas_addition={}, has_deletion={}", has_addition, has_deletion);
+
+   // if has_addition && has_deletion {
+/*
+        results.push(Output {
+		    old_commit: old.id(),
+		    new_commit: new.id(),
+		    origin_line: 0,
+		    destination_line: 0,
+		    num_lines: 0
+        });
+*/
+    //}
+
+
+    if !has_addition || !has_deletion {
+        println!("Does not have addition or deletion.");
+        Ok(vec![])  // Return empty vector.
+    } else {
+
+	    Ok(vec![Output {
+		    old_commit: old.id(),
+		    new_commit: new.id(),
+		    origin_line: 0,
+		    destination_line: 0,
+		    num_lines: 0
+	    }])
+    }
+
+   // Ok(results)
 }
 
 	
